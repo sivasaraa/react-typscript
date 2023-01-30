3f6645d33afb6f413f114065eec4a92f616c1c6b
saravanan <s.k.saravanan94@gmail.com>
2023-01-30 19:58:36 +0530

    nested child react.reactNode

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import './App.css';
 import { Greet } from './Component/Greet';
 import { Status } from './Component/Status';
 import { Heading } from './Component/Heading';
+import { Oscar } from './Component/Oscar';
 
 function App() {
   return (
     <div className="App">
       {/* <Greet name='World' count={10} loggedin={true}/> */}
       {/* <Status status='success'/> */}
-      <Heading>Hello World</Heading>
+      {/* <Heading>Hello World</Heading> */}
+      <Oscar><Heading>Oscar Hero = Leonardo Dicaprio</Heading></Oscar>
     </div>
   );
 }
