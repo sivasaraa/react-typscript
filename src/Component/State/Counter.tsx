7c41888265b6f5030b8b70f5c3d1dafc34ecb5ba
saravanan <s.k.saravanan94@gmail.com>
2023-02-03 19:34:33 +0530

    specified type in typeprops

--- src/Component/State/Counter.tsx
+++ src/Component/State/Counter.tsx
@@ -4,33 +4,41 @@ const initialState = {
     count : 0
 }
 
-type stateprops = {
+type stateProps = {
     count : number
 }
 
-type actionprops = {
-    type: string,
+type actionProps = {
+    type : 'increment' | 'decrement',
     payload : number
 }
 
-const reducer = (state:stateprops,action:actionprops) => {
+type resetProps = {
+    type : 'reset'
+}
+
+const reducer = (state : stateProps, action : actionProps | resetProps) => {
     switch(action.type){
         case 'increment':
             return {count : state.count + action.payload}
         case 'decrement':
             return {count : state.count - action.payload}
+        case 'reset':
+            return initialState
         default:
             return state
     }
 }
 
 export const Counter = () => {
-    const [newstate, dispatch] = useReducer(reducer,initialState)
+    const [newState, dispatch] = useReducer(reducer, initialState)
+
     return (
         <div>
-            <div>{newstate.count}</div>
-            <button onClick={() => dispatch({type:'increment',payload:10})}>Increment 10</button>
-            <button onClick={() => dispatch({type:'decrement',payload:10})}>Decrement 10</button>
+            <div>{newState.count}</div>
+            <button onClick={() => dispatch({type:'increment',payload:5})}>Increment Count</button>
+            <button onClick={() => dispatch({type:'decrement',payload:5})}>Decrement Count</button>
+            <button onClick={() => dispatch({type:'reset'})}>Reset</button>
         </div>
     )
 }
